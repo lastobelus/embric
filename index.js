46bec8a1bf3b473b9ed43694fcdb38889ad0311c
Michael Johnston <lastobelus@mac.com>
2015-11-17 11:11:45 -0800

    formatting

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   
   included: function(app) {
-      this._super.included(app);
-      app.import(app.bowerDirectory + '/fabric/dist/fabric.js');
-    }
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/fabric/dist/fabric.js');
+  }
 };
