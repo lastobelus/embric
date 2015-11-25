49ba1cb3f2f8216d002a0bf6e04de4517cbbefa5
Michael Johnston <lastobelus@mac.com>
2015-11-24 19:59:10 -0800

    initial version of api docs, not much there yet

---
+++ api/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Components.EmbricEditor",
+        "Components.FabricCanvas"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
