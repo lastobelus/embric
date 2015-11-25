e81a4cb06d93b5c9eade5f834f42ecade258afe8
Michael Johnston <lastobelus@mac.com>
2015-11-25 14:04:13 -0800

    WIP: more docs

--- addon/components/embric-basic-shapes-widget/component.js
+++ addon/components/embric-basic-shapes-widget/component.js
@@ -2,6 +2,20 @@ import Ember from 'ember';
 import layout from './template';
 import  ShapesSupport from 'embric/mixins/shapes-support';
 
+/**
+  Widget that provides a `select` with a list of shapes. When the user clicks
+  `Add`, the shape is added to the canvas.
+
+  ### Follows Selection
+  If there is an active selection, the shape will be drawn with attributes
+  taken from it, and positioned on top of it. Otherwise the shape will be
+  drawn in the center of the canvas with defaults of fill:black & sized to
+  fit in a 50px square bounds.
+
+  @element embric-basic-shapes-widget
+  @uses ShapesSupport
+*/
+
 export default Ember.Component.extend(ShapesSupport, {
   layout,
   name: 'Add Shape',
--- addon/components/embric-static-text-editor/component.js
+++ addon/components/embric-static-text-editor/component.js
@@ -2,11 +2,33 @@ import Ember from 'ember';
 import layout from './template';
 import  TextSupport from 'embric/mixins/text-support';
 
+/**
+  Widget that provides a `textarea` for entering/editing text, and a `select`
+  for choosing a font.
+
+  It expects fonts to be bound to `fontList` in the following form:
+
+  ```javascript
+  Ember.A([
+    { cssName: 'Arial Black', displayName: 'Arial Black' },
+    { cssName: 'Helvetica', displayName: 'Helvetica' }
+  ])
+  ```
+
+  @element embric-static-text-editor
+  @uses ShapesSupport
+*/
+
 export default Ember.Component.extend(TextSupport, {
   layout,
   name: 'Basic Text',
   animate: true,
   text: '',
+  /**
+    The list of available fonts.
+
+    @property
+  */
   fontList: Ember.A([
     { cssName: 'Arial Black', displayName: 'Arial Black' },
     { cssName: 'Helvetica', displayName: 'Helvetica' },
