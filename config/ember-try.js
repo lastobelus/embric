51faae20fa5eed9789b7fd8a9314bdf6a49d7b3c
Michael Johnston <lastobelus@mac.com>
2016-02-16 19:00:01 -0800

    upgrade to ember-cli 2.3

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,42 +3,52 @@ module.exports = {
   scenarios: [
     {
       name: 'default',
-      dependencies: { }
+      bower: {
+        dependencies: { }
+      }
     },
     {
       name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
       }
     },
     {
       name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
       }
     },
     {
       name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
       }
     },
     {
       name: 'Ember 1.13 with ember-data',
-      dependencies: {
-        'ember': '1.13.8',
-        'ember-data': '1.13.15'
+      bower: {
+        dependencies: {
+          'ember': '1.13.8',
+          'ember-data': '1.13.15'
+        }
       }
     },
-    
+
   ]
 };
